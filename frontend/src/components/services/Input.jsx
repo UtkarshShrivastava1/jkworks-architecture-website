1de2c795094db5e70a5c2ba7b09b6793e7acdd0e
Abhay0410 <kumarsinghabhay77@gmail.com>
2025-07-11 21:04:34 +0530

    remove useless file

--- frontend/src/components/services/Input.jsx
+++
@@ -1,7 +0,0 @@
-const Input = ({ type, placeholder, value, onChange }) => (
-    <input type={type} placeholder={placeholder} value={value}
-      onChange={onChange} className="input" required />
-  );
-  
-  export default Input;
-  
